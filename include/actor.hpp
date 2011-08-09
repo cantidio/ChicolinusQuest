c1036ccc9b3e3f318d9c310559fc9634ad09a8b0
Cantidio Fontes <aniquilatorbloody@gmail.com>
2011-08-09 02:13:38 +0000

    - Classe actor que tinha esquecido de enviar.  - Remoção do som de passos antigo.

---
+++ include/actor.hpp
@@ -0,0 +1,51 @@
+#ifndef _ACTOR_
+#define _ACTOR_
+#include "object.hpp"
+
+class Actor : public Object
+{
+	protected:
+		float mLife;		/**<< actual life of the actor*/
+		float mLifeMax;		/**<< the max amount of life the actor can have*/
+	public:
+		Actor
+		(
+			const Point&		pPosition,
+			const std::string&	pScript,
+			BG*					pBG
+		);
+
+		inline bool isDead() const
+		{
+			return (mLife <= 0 );
+		}
+
+		inline int getLife() const
+		{
+			return mLife;
+		}
+
+		inline float getLifeMax() const
+		{
+			return mLifeMax;
+		}
+
+		inline void subLife(const float& pLife)
+		{
+			mLife -= pLife;
+			if( mLife < 0)
+			{
+				mLife = 0;
+			}
+		}
+
+		inline void addLife(const float& pLife)
+		{
+			mLife += pLife;
+			if( mLife > mLifeMax )
+			{
+				mLife = mLifeMax;
+			}
+		}
+};
+#endif
---
+++ src/actor.cpp
@@ -0,0 +1,11 @@
+#include "actor.hpp"
+#include "lua.hpp"
+using namespace Gorgon::Script;
+
+Actor::Actor( const Point& pPosition, const std::string& pScript, BG* pBG )
+	: Object(pPosition, pScript, pBG)
+{
+	Lua lua( pScript );
+	mLife		= lua.getNumericVar("life");
+	mLifeMax	= lua.getNumericVar("lifeMax");
+}
